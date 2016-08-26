4d6ff67da994de2436e3bcc0eb1cdace8061bf76
ruhiel_murrue <ruhiel1983@gmail.com>
2016-08-26 11:29:49 +0900

    初回コミット

---
+++ WpfApplication2/DelegateCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApplication2
+{
+	public class DelegateCommand : ICommand
+	{
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+		public event EventHandler CanExecuteChanged;
+		public void Execute(object parameter)
+		{
+			MessageBox.Show(string.Format("コマンドが実行されました: {0}", parameter.ToString()));
+		}
+	}
+}
---
+++ WpfApplication2/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2
+{
+	public class ViewModel
+	{
+		public List<MenuItemModel> SomeCollection { get; set; }
+		public ViewModel()
+		{
+			SomeCollection = new List<MenuItemModel>()
+			{
+				new MenuItemModel() {Command = new DelegateCommand(), DisplayName = "長門" },
+				new MenuItemModel() {Command = new DelegateCommand(), DisplayName = "陸奥" },
+				new MenuItemModel() {Command = new DelegateCommand(), DisplayName = "金剛" },
+				new MenuItemModel() {Command = new DelegateCommand(), DisplayName = "比叡" },
+				new MenuItemModel() {Command = new DelegateCommand(), DisplayName = "榛名" },
+				new MenuItemModel() {Command = new DelegateCommand(), DisplayName = "霧島" },
+			};
+		}
+	}
+}
